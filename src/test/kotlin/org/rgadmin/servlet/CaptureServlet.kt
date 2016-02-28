bba50dc6099e51bafeb73a12cda2be19472f79c6
Jussi Jartamo <jussi.jartamo@gofore.com>
2016-02-28 12:21:12 +0200

    Unit test updated with API change

--- src/test/kotlin/org/rgadmin/servlet/CaptureServlet.kt
+++ src/test/kotlin/org/rgadmin/servlet/CaptureServlet.kt
@@ -1,6 +1,7 @@
 package org.rgadmin.servlet
 
 import org.rgadmin.servlet.api.v1.ApiV1Servlet
+import org.rgadmin.servlet.api.v1.Connection
 import org.rgadmin.servlet.api.v1.Result
 import java.util.*
 
@@ -8,7 +9,7 @@ class CaptureServlet : ApiV1Servlet() {
 
     var lastScript = ""
 
-    override fun executeScript(script: String): Result {
+    override fun executeScript(script: String, connection: Connection?): Result {
         lastScript = script
         Thread.sleep(1000L)
         return Result(Collections.emptyList())
